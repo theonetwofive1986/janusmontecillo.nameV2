9638cd37a5ce7cc92f6ba9ccf142b0af31b45e70
theonetwofive1986 <145209374+theonetwofive1986@users.noreply.github.com>
2024-11-11 08:34:53 +0800

    update
    js

--- js/bootstrap-4.4.1.js
+++ js/bootstrap-4.4.1.js
@@ -4519,3 +4519,26 @@
 
 })));
 //# sourceMappingURL=bootstrap.js.map
+
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+
+function getTodaysDate() {
+ 
+ var now = new Date();
+ var days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+ var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+ var date = ((now.getDate() < 10) ? "0" : "") + now.getDate();
+
+ var today = days[now.getDay()] + ", " +
+  months[now.getMonth()] + " " +
+  date + ", " +
+  now.getFullYear();
+
+ return (today);
+}
